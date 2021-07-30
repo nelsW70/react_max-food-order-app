02cf4f2777668d58e6db734b246ed1951603808f
Nelson Foltz <nelsfoltz@gmail.com>
2021-07-30 08:42:17 -0400

    add: setting up form values

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -8,7 +8,7 @@ const Backdrop = props => {
 const ModalOverlay = props => {
   return (
     <div className={classes.modal}>
-      <div className={classes.context}>{props.children}</div>
+      <div className={classes.content}>{props.children}</div>
     </div>
   );
 };
